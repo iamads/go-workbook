1bd8b01d3231f8ca6d11baf4ff2444d1b471c9d9
Abhijeet <abhijeet@hyperlearn.in>
2026-06-04 23:08:35 +0530

    codex fix pprof file cration

--- prime_compute/main.go
+++ prime_compute/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	pprof "runtime/pprof"
 )
@@ -9,39 +10,89 @@ import (
 func main() {
 	testValue := 1000_000
 
-	f, _ := os.Create("pprof_computePrimeSingle")
-	pprof.StartCPUProfile(f)
-	computePrimeSingle(testValue)
-	pprof.StopCPUProfile()
-	fmt.Println("computed prime single")
+	profiles := []struct {
+		fileName string
+		label    string
+		compute  func()
+	}{
+		{
+			fileName: "pprof_computePrimeSingle",
+			label:    "computed prime single",
+			compute:  func() { computePrimeSingle(testValue) },
+		},
+		{
+			fileName: "pprof_computePrimesWithGoroutines",
+			label:    "computed primes with goroutines",
+			compute:  func() { computePrimesWithGoroutines(testValue) },
+		},
+		{
+			fileName: "pprof_computePrimesWithGoroutinesWithBoundedConcurrency",
+			label:    "computed primes with goroutines and bounded concurrency",
+			compute:  func() { computePrimesWithGoroutinesBoundedConcurrency(testValue) },
+		},
+		{
+			fileName: "pprof_computePrimeGoroutinesAtomic",
+			label:    "computed prime goroutines atomic",
+			compute:  func() { computePrimeGoroutinesAtomic(testValue) },
+		},
+		{
+			fileName: "pprof_computePrimeGoroutinesAtomicBounded",
+			label:    "computed prime goroutines atomic bounded",
+			compute:  func() { computePrimeGoroutinesAtomicBounded(testValue) },
+		},
+		{
+			fileName: "pprof_computePrimeChunked",
+			label:    "computed prime chunked",
+			compute:  func() { computePrimeChunked(testValue) },
+		},
+		{
+			fileName: "pprof_computePrimeChunkedAtomic",
+			label:    "computed prime chunked atomic",
+			compute:  func() { computePrimeChunkedAtomic(testValue) },
+		},
+	}
 
-	f1, _ := os.Create("pprof_computePrimesWithGoroutines")
-	pprof.StartCPUProfile(f1)
-	computePrimesWithGoroutines(testValue)
-	pprof.StopCPUProfile()
-	fmt.Println("computed primes with gorroutines")
+	for _, profile := range profiles {
+		if err := runCPUProfile(profile.fileName, profile.compute); err != nil {
+			log.Fatalf("failed to write %s: %v", profile.fileName, err)
+		}
+		fmt.Println(profile.label)
+	}
+}
 
-	f2, _ := os.Create("pprof_computePrimesWithGoroutinesWithBoundedConcurrency")
-	pprof.StartCPUProfile(f2)
-	computePrimesWithGoroutinesBoundedConcurrency(testValue)
-	pprof.StopCPUProfile()
-	fmt.Println("computed primes with gorroutines and bouded concurrecy")
+func runCPUProfile(fileName string, compute func()) (err error) {
+	f, err := os.Create(fileName)
+	if err != nil {
+		return fmt.Errorf("create profile file: %w", err)
+	}
 
-	f3, _ := os.Create("pprof_computePrimeGoroutinesAtomic")
-	pprof.StartCPUProfile(f3)
-	computePrimeGoroutinesAtomic(testValue)
-	pprof.StopCPUProfile()
-	fmt.Println("computed PrimeGoroutinesAtomic")
+	profileStarted := false
+	fileClosed := false
+	defer func() {
+		if profileStarted {
+			pprof.StopCPUProfile()
+		}
+		if !fileClosed {
+			if closeErr := f.Close(); err == nil && closeErr != nil {
+				err = fmt.Errorf("close profile file: %w", closeErr)
+			}
+		}
+	}()
 
-	f4, _ := os.Create("pprof_computePrimeGoroutinesAtomicBounded")
-	pprof.StartCPUProfile(f4)
-	computePrimeGoroutinesAtomicBounded(testValue)
-	pprof.StopCPUProfile()
-	fmt.Println("computed Prime GoroutinesAtomic Bounded")
+	if err := pprof.StartCPUProfile(f); err != nil {
+		return fmt.Errorf("start cpu profile: %w", err)
+	}
+	profileStarted = true
 
-	f5, _ := os.Create("pprof_computePrimeChuked")
-	pprof.StartCPUProfile(f5)
-	computePrimeChunked(testValue)
+	compute()
+
 	pprof.StopCPUProfile()
-	fmt.Println("computed Prime Chunked")
+	profileStarted = false
+
+	if err := f.Close(); err != nil {
+		return fmt.Errorf("close profile file: %w", err)
+	}
+	fileClosed = true
+
+	return nil
 }
